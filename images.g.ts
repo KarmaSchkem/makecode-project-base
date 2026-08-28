3127ba4cac24f2caac3824d7152202ba3279b748
KarmaSchkem <151461062+KarmaSchkem@users.noreply.github.com>
2026-08-28 23:39:10 +0200

    Mise à jour pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,51 @@
+// Code généré automatiquement. Ne pas modifier.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "cursor":return img`
+. f f . . . . . f f f . . . . . 
+f 1 1 f . . f f b 1 1 f . . . . 
+f 1 1 1 f f b 1 f b 1 1 f . . . 
+f b 1 1 1 1 f b 1 1 1 1 f . . . 
+. f b 1 1 1 1 1 1 1 c 1 1 f . . 
+. . f b 1 1 1 1 c 1 1 c 1 f . . 
+. . f f b 1 1 1 1 c 1 1 1 f . . 
+. . f c c b 1 1 1 1 1 1 1 f f . 
+. . f c b b 1 1 1 1 1 1 f 5 5 f 
+. . . f b b 1 1 1 1 1 f 5 5 5 f 
+. . . . f b b b b b f 5 5 5 f . 
+. . . . . f f f f f 5 5 5 f . . 
+. . . . . . . . f 4 5 5 f . . . 
+. . . . . . . . f 4 4 f . . . . 
+. . . . . . . . . f f . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Code généré automatiquement. Ne pas modifier.
--- main.ts
+++ main.ts
@@ -1,1 +1,4 @@
- 
+namespace userconfig {
+    export const ARCADE_SCREEN_WIDTH = 192
+    export const ARCADE_SCREEN_HEIGHT = 144
+}
